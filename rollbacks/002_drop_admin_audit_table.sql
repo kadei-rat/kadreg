e7622c5ed6d0ed9eec5aede0f117522ca11cbe28
Simon Woolf <simon.woolf@ably.com>
2025-11-16 15:40:13 -0500

    Move db setup into single setup file
    no point storing separate migrations until we have a production db

--- rollbacks/002_drop_admin_audit_table.sql
+++
@@ -1,4 +0,0 @@
--- Rollback for 002_create_admin_audit_table.sql
-
-DROP TABLE IF EXISTS admin_audit_log;
-DROP TYPE IF EXISTS admin_action_type;
--- rollbacks/003_drop_pending_members_table.sql
+++
@@ -1,6 +0,0 @@
--- Rollback for 003_create_pending_members_table.sql
-
-DROP TRIGGER IF EXISTS prevent_duplicate_email_in_pending_members ON pending_members;
-DROP FUNCTION IF EXISTS check_pending_member_email();
-DROP INDEX IF EXISTS idx_pending_members_created_at;
-DROP TABLE IF EXISTS pending_members;
